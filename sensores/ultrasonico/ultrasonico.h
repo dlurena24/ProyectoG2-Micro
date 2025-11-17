76b9849b7beae90b3dc004ed433e38812535b452
Marco <111095859+Specktic@users.noreply.github.com>
2025-11-17 00:24:56 -0600

    Definir headers de ultrasonico

---
+++ sensores/ultrasonico/ultrasonico.h
@@ -0,0 +1,27 @@
+#ifndef ULRTASONICO_H
+#define ULTRASONICO_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// pines
+#define GPIO_TRIG 5
+#define GPIO 18
+
+// inicializa el modulo
+void ultrasonico_init(void);
+
+// mide la distancia
+void ultrasonico_medicion(void);
+
+// mide duracion del pulso
+uint64_t ultrasonico_medicion_raw(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // Ultrasonico_H
--- sensores/ultrasonico/ultrasonico_timer.h
+++ sensores/ultrasonico/ultrasonico_timer.h
@@ -3,13 +3,18 @@
 
 #include <stdint.h>
 
-// Inicializa Timer Group 0, Timer 0 como contador libre a ~1 MHz (1 us por tick)
+#ifdef __cplusplus
+extern  "C" {
+#endif
+
+// Inicializa temporizador
 void ultrasonico_timer_init(void);
 
-// Devuelve el valor actual del timer en microsegundos
+// Retorna timestamp
 uint64_t ultrasonico_timer_get_us(void);
 
-// Pone el contador en cero
-void ultrasonico_timer_reset(void);
+#ifdef __cplusplus
+}
+#endif
 
-#endif // ULTRASONICO_TIMER_H
+#endif
